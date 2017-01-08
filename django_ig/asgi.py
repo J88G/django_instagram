def97b23ea90cfd71137f0428ad1128f1a0fb2de
andy <andyalam@hotmail.com>
2017-01-07 16:46:23 -0800

    deployment

--- django_ig/asgi.py
+++ django_ig/asgi.py
@@ -1,5 +1,5 @@
 import os
 import channels.asgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_ig.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-ig.settings")
 channel_layer = channels.asgi.get_channel_layer()
